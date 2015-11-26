65a2d709b75bb0e98b7d75f4ac7a56082e31c354
Paweł Nowak <pawel@livetalk.lol>
2015-11-26 02:07:52 -0800

    Bugfix: enabling a bound fiber would cause an infinite loop.

--- fiberize/src/fiberize/scheduler.cpp
+++ fiberize/src/fiberize/scheduler.cpp
@@ -23,6 +23,15 @@ void Scheduler::enable(detail::ControlBlock* controlBlock, boost::unique_lock<de
          */
         assert(dynamic_cast<detail::FiberControlBlock*>(controlBlock) != nullptr);
         enableFiber(static_cast<detail::FiberControlBlock*>(controlBlock), std::move(lock));
+    } else if (controlBlock->bound == this) {
+        /**
+         * If the control block is bound, it means it must be a fiber, because a
+         * thread scheduler would override the enable() function and intercept
+         * the thread.
+         * TODO: refactor, this is not nice
+         */
+        assert(dynamic_cast<detail::FiberControlBlock*>(controlBlock) != nullptr);
+        enableFiber(static_cast<detail::FiberControlBlock*>(controlBlock), std::move(lock));
     } else {
         /**
          * Forward the control block to its bound scheduler.
